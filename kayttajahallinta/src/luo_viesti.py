a1f97b9771d699d52fb81025859ecb20221f6329
katiheikkonen <kati.m.heikkonen@gmail.com>
2020-09-15 09:07:18 +0300

    ses funktiot aluille

--- kayttajahallinta/src/luo_viesti.py
+++ kayttajahallinta/src/luo_viesti.py
@@ -2,14 +2,17 @@
 
 import boto3
 
-ses = boto3.client('ses')
+ses = boto3.client('ses', region_name='eu-central-1')
 
-def send_ses(event, context):
+def create_message():
     response = ses.create_custom_verification_email_template(
-        TemplateName='MyStockApp-verification',
+        TemplateName='MyStocksApp-verification',
         FromEmailAddress='tsttestitesti@gmail.com',
         TemplateSubject='Confirm your registration to MyStock-app',
-        TemplateContent='MOIKKELIS',
-        SuccessRedirectionURL='string', #The URL that the recipient of the verification email is sent to if his or her address is successfully verified.
-        FailureRedirectionURL='string' #The URL that the recipient of the verification email is sent to if his or her address is not successfully verified.
+        TemplateContent='Moikkelis',
+        SuccessRedirectionURL='http://nomnomproductions.com', #The URL that the recipient of the verification email is sent to if his or her address is successfully verified.
+        FailureRedirectionURL='http://nomnomproductions.com' #The URL that the recipient of the verification email is sent to if his or her address is not successfully verified.
     )
+
+create_message()
+
