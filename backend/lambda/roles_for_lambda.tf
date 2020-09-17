26b8fa074856e26ca8c3c172fd6695267d36fa0d
MikkoGit <leivo.mikko@gmail.com>
2020-09-17 13:38:46 +0300

    loop poistettu

--- backend/lambda/roles_for_lambda.tf
+++ backend/lambda/roles_for_lambda.tf
@@ -9,3 +9,22 @@ resource "aws_iam_role_policy_attachment" "lambda_analyze_with_comprehend_attach
   role       = aws_iam_role.role_for_lambda_analyze_with_comprehend.name
   policy_arn = aws_iam_policy.lambda_analyze_with_comprehend.arn
 }
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = module.s3_moduulit.customer_reviews_s3_bucket_id
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.dummy_lambda_func.arn
+    events = ["s3:ObjectCreated:*"
+    ]
+    #filter_prefix       = "AWSLogs/"
+    #filter_suffix       = ".log"
+  }
+}
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.dummy_lambda_func.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = module.s3_moduulit.customer_reviews_s3_bucket_arn
+}
--- backend/s3/customer_reviews_s3.tf
+++ backend/s3/customer_reviews_s3.tf
@@ -1,6 +1,3 @@
-module "dummy_arn" {
-  source = "../lambda/"
-}
 
 resource "aws_s3_bucket" "customer_reviews_s3" {
   bucket = var.s3_bucket_name
@@ -17,24 +14,4 @@ output "customer_reviews_s3_bucket_arn" {
 
 output "customer_reviews_s3_bucket_id" {
   value = aws_s3_bucket.customer_reviews_s3.arn
-}
-
-
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.customer_reviews_s3.id
-  lambda_function {
-    lambda_function_arn = module.dummy_arn.lambda_dummy_arn
-    events = ["s3:OdjectCreated:*"
-    ]
-    #filter_prefix       = "AWSLogs/"
-    #filter_suffix       = ".log"
-  }
-}
-
-resource "aws_lambda_permission" "allow_bucket" {
-  statement_id  = "AllowExecutionFromS3Bucket"
-  action        = "lambda:InvokeFunction"
-  function_name = module.dummy_arn.lambda_dummy_arn
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.customer_reviews_s3.arn
 }
