8fbff3f3f288a83cab9f6e7389d18bbed74d8b59
MikkoGit <leivo.mikko@gmail.com>
2020-09-17 15:12:54 +0300

    policy fiksattu

--- backend/lambda/policies_for_lambda.tf
+++ backend/lambda/policies_for_lambda.tf
@@ -3,15 +3,15 @@ module "s3_moduulit" {
   source = "../s3/"
 }
 
-resource "aws_iam_policy" "s3_get_and_cw_log" {
+resource "aws_iam_policy" "lambda_analyze_with_comprehend" {
   name        = "lambda_analyze_with_comprehend_policy"
   description = "IAM policy with BasicLambdaExecution role and Amazon Comprehend full access"
 
-  policy = data.aws_iam_policy_document.s3_get_and_cw_log.json
+  policy = data.aws_iam_policy_document.analyze_with_comprehend.json
 }
 
 #Policy document ylempää POST metodia varten:
-data "aws_iam_policy_document" "s3_get_and_cw_log" {
+data "aws_iam_policy_document" "analyze_with_comprehend" {
   statement {
     sid = "1as"
     effect = "Allow"
@@ -20,29 +20,13 @@ data "aws_iam_policy_document" "s3_get_and_cw_log" {
       "dynamodb:PutItem",
       "logs:CreateLogGroup",
       "logs:CreateLogStream",
-      "logs:PutLogEvents"
+      "logs:PutLogEvents",
+      "comprehend:DetectSentiment"
     ]
     resources = [
       "${module.s3_moduulit.customer_reviews_s3_bucket_arn}/*",
       "arn:aws:logs:*:*:*",
+      "*"
     ]
-  }
-}
-
-resource "aws_iam_policy" "lambda_analyze_with_comprehend" {
-  name        = "lambda_analyze_with_comprehend_policy"
-  description = "IAM policy with BasicLambdaExecution role and Amazon Comprehend full access"
-
-  policy = data.aws_iam_policy_document.analyze_with_comprehend.json
-}
-
-data "aws_iam_policy_document" "analyze_with_comprehend" {
-  statement {
-    sid = "2as"
-    effect = "Allow"
-    actions = [
-      "comprehend:DetectSentiment"
-    ]
-    resources = ["*"]
   }
 }
