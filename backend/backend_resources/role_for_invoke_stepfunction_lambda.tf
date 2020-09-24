ee1b638e4d4145817965e699c090d2749654efd2
katiheikkonen <kati.m.heikkonen@gmail.com>
2020-09-24 10:34:44 +0300

    siirto

--- backend/backend_resources/role_for_invoke_stepfunction_lambda.tf
+++ backend/backend_resources/role_for_invoke_stepfunction_lambda.tf
@@ -23,19 +23,3 @@ resource "aws_iam_role_policy_attachment" "lambda_invoke_step_function_attachmen
   policy_arn = aws_iam_policy.lambda_invoke_stepfunction_policy.arn
 }
 
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.customer_review_s3.id
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.invoke_stepfunction.arn
-    events = ["s3:ObjectCreated:Put"]
-  }
-   depends_on = [aws_lambda_permission.allow_bucket]
-}
-
-resource "aws_lambda_permission" "allow_bucket" {
-  statement_id  = "AllowExecutionFromS3Bucket"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.invoke_stepfunction.arn
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.customer_review_s3.arn
-}
--- backend/backend_resources/s3_customer_reviews.tf
+++ backend/backend_resources/s3_customer_reviews.tf
@@ -8,6 +8,24 @@ resource "aws_s3_bucket" "customer_review_s3" {
   }
 }
 
+#  Luodaan bucket notification, jotta invoke_stepfuntion lambda käynnistyy kun bucketiin laitetaan uusi arvostelu
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.customer_review_s3.id
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.invoke_stepfunction.arn
+    events = ["s3:ObjectCreated:Put"]
+  }
+   depends_on = [aws_lambda_permission.allow_bucket]
+}
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.invoke_stepfunction.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.customer_review_s3.arn
+}
+
 output "customer_reviews_s3_bucket_arn" {
   value = aws_s3_bucket.customer_review_s3.arn
 }
