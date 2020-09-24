7188efacf3804612b2d538d86c8dab054741dd17
katiheikkonen <kati.m.heikkonen@gmail.com>
2020-09-24 12:00:03 +0300

    lambdamuutos

--- backend/src/get_from_s3.py
+++ backend/src/get_from_s3.py
@@ -1,27 +1,21 @@
 import json
-import uuid
-
 import boto3
-
 
 s3 = boto3.resource('s3')
 
 def get_object_from_s3(event, context):
-    message = "TERRIBLE! I hate this piece of shit. The worst product I have ever bought! I will make sure you guys go out of business soon!!!"
-    return {"body": json.dumps(message)}
-
-
 #Poimitaan S3sta tulevasta eventistä..
-#     s3_trigger = event['Records']
-#     # Uuden tiedoston nimi, sekä
-#     s3_filename = s3_trigger[0]['s3']['object']['key']
-#     # bucketin nimi
-#     s3_bucket_name = s3_trigger[0]['s3']['bucket']['name']
-#
-#     # haettu objekti:
-#     obj = s3.Object(s3_bucket_name, s3_filename)
-#     # tiedoston sisältä
-#     m_body = obj.get()['Body']._raw_stream.readline()
-#     viesti = json.loads(m_body)
-#
-#     return {"body":json.dumps(viesti)}
+     s3_trigger = event['Records']
+     # Uuden tiedoston nimi, sekä
+     s3_filename = s3_trigger[0]['s3']['object']['key']
+     # bucketin nimi
+     s3_bucket_name = s3_trigger[0]['s3']['bucket']['name']
+     # haettu objekti:
+     obj = s3.Object(s3_bucket_name, s3_filename)
+     # tiedoston sisältä
+     m_body = obj.get()['Body']._raw_stream.readline()
+     viesti = json.loads(m_body)
+
+     return {
+         "body": json.dumps(viesti['message'])
+     }
--- business_intelligence/business_intelligence_resources/vars.tf
+++ business_intelligence/business_intelligence_resources/vars.tf
@@ -6,9 +6,9 @@ variable "environmental_table_name" {
 }
 # DynamoDB eu-central-1 stream arn put_to_analysis_s3 lambdaa varten
 variable "global_table_stream_arn" {
-  default: "arn:aws:dynamodb:eu-central-1:821383200340:table/sentiment_data_analysis_global_table/stream/2020-09-24T07:52:56.372"
+  default = "arn:aws:dynamodb:eu-central-1:821383200340:table/sentiment_data_analysis_global_table/stream/2020-09-24T07:52:56.372"
 }
 # DynamoDB eu-central-1 arn
 variable "global_table_eu_central_1_arn" {
-  default: "arn:aws:dynamodb:eu-central-1:821383200340:table/sentiment_data_analysis_global_table"
+  default = "arn:aws:dynamodb:eu-central-1:821383200340:table/sentiment_data_analysis_global_table"
 }
